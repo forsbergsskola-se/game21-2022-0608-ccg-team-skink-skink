6c591d0b3524511f03bd382622ab028aed7ad090
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-14 10:49:03 +0200

    Add DistanceCheck

--- Assets/Scripts/Gameplay/AI/Unit/UnitAI.cs
+++ Assets/Scripts/Gameplay/AI/Unit/UnitAI.cs
@@ -9,13 +9,23 @@ namespace Gameplay.AI.Unit
         //Todo: move stats to another component
         [SerializeField] private float speed;
         [SerializeField] private Vector3 direction;
+        [SerializeField] private float range;
         
         private Movement movement;
 
         private void Awake() 
             => movement = new Movement(speed, direction);
-        
-        private void FixedUpdate() 
-            => StartCoroutine(movement.Move(this.transform));
+
+        private void FixedUpdate()
+        {
+            RaycastHit hit;
+            
+            if(Physics.SphereCast(transform.position, 1, direction,out hit, range))
+            {
+                
+            } 
+            else StartCoroutine(movement.Move(this.transform));
+        } 
+           
     }
 }
