dc044e8612563fde8d98929b7bb528adb9cc3217
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-14 14:31:26 +0200

    Implemented LevelSystem Interfaces
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

---
+++ Assets/Scripts/Meta/Interfaces/ILevelSystem.cs
@@ -0,0 +1,14 @@
+using UnityEngine.SceneManagement;
+namespace Meta.Interfaces
+{
+    public interface ILevelController
+    {
+        public void LoadLevel(ILevel level);
+    }
+    public interface ILevel
+    {
+        public string Name { get; }
+        public int Index { get; }
+        public Scene LevelScene { get; }
+    }
+}
