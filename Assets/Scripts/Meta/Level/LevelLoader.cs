e730c7577f7a7aecd585c8554fa69ca3527ecef6
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-15 15:52:55 +0200

    Added comment on how to improve LevelLoad

--- Assets/Scripts/Meta/Level/LevelLoader.cs
+++ Assets/Scripts/Meta/Level/LevelLoader.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 namespace Meta.Level
 {
+    //TODO: Maybe dont use MonoBehaviour if we want to load next scene when we complete a level instead of going back to map menu
     public class LevelLoader : MonoBehaviour, ILevelController
     {
         public void LoadLevel(ILevel level)
         {
             SceneManager.LoadScene(level.InternalSceneName);
         }
+        //TODO:Dont destroy on load
     }
 }
