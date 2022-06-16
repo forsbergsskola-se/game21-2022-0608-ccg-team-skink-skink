f8fe0566371ba22689c22f533f555fb8c9b0985e
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-16 17:06:33 +0200

    WIIP refactoring
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

--- Assets/Scripts/Meta/InventorySystem/CardHandSO.cs
+++ Assets/Scripts/Meta/InventorySystem/CardHandSO.cs
@@ -6,16 +6,18 @@ namespace Meta.InventorySystem
     [CreateAssetMenu(fileName = "NewCardHand", menuName = "CardSystem/Cardhand")]
     public class CardHandSO : ScriptableObject, ICardHand
     {
-        [SerializeField] private CardSO abilityCard;
-        [SerializeField] private CardSO[] cards = new CardSO[6];
+        // [SerializeField] private CardSO abilityCard;
+        // [SerializeField] private CardSO[] cards = new CardSO[6];
         
-        public void Awake()
-        {
-            AbilityCard = abilityCard;
-            Cards = cards;
-        }
+        // public void Awake()
+        // {
+        //     AbilityCard = abilityCard;
+        //     Cards = cards;
+        // }
 
-        public ICard[] Cards { get; private set; }
+        public ICard[] Cards { get; } = new ICard[6];
+
+        // public ICard[] Cards { get; private set; }
         public ICard AbilityCard { get; set; }
     }
 }
