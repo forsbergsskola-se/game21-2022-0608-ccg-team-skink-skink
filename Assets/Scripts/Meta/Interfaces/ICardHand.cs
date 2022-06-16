d9b751c76f08b3516dd5688fcc9b9266472dbf2f
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-16 11:39:56 +0200

    Added XML documentation to the interfaces
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/Interfaces/ICardHand.cs
+++ Assets/Scripts/Meta/Interfaces/ICardHand.cs
@@ -1,8 +1,17 @@
 namespace Meta.Interfaces
 {
+    /// <summary>
+    /// Interface for the deck that the player and enemy uses in gameplay
+    /// </summary>
     public interface ICardHand
     {
+        /// <summary>
+        /// Holds the current hand of cards
+        /// </summary>
         public ICard[] Cards { get; }
+        /// <summary>
+        /// Holds the special ability card
+        /// </summary>
         public ICard AbilityCard { get; }
     }
 }
--- Assets/Scripts/Meta/Interfaces/ICurrency.cs
+++ Assets/Scripts/Meta/Interfaces/ICurrency.cs
@@ -1,10 +1,15 @@
 namespace Meta.Interfaces
 {
+    /// <summary>
+    /// Contains the normal coins that the player has
+    /// </summary>
     public interface INormalCoinCarrier
     { 
         public int Amount { get; }
     }
-
+    /// <summary>
+    /// Contains the premium coins that the player has
+    /// </summary>
     public interface IPremiumCoinCarrier
     {
         public int Amount { get; }
