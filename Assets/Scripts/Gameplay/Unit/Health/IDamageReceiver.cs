f90d534167280fecf18a7e7d3483b299a75404d4
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-16 17:29:51 +0200

    Changed IDamageReceiver to correct namespace
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Gameplay/Unit/Health/IDamageReceiver.cs
+++ Assets/Scripts/Gameplay/Unit/Health/IDamageReceiver.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageReciver
+namespace Gameplay.Unit.Health
 {
-    void TakeDamage(float value);
+    public interface IDamageReciver
+    {
+        void TakeDamage(float value);
+    }
 }
