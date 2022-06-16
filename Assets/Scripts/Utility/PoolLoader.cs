f459f56ee42025ca688c484bdee6ec6f2daca520
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-16 18:26:19 +0200

    Refactored PoolLoader

--- Assets/Scripts/Utility/PoolLoader.cs
+++ Assets/Scripts/Utility/PoolLoader.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Meta.Interfaces;
-using Meta.InventorySystem;
 using UnityEngine;
 
 namespace Utility
@@ -8,33 +7,35 @@ namespace Utility
     public class PoolLoader : MonoBehaviour
     {
         [SerializeField] private PoolSO pool;
-        [SerializeField, RequireInterface(typeof(ICardHand))] private Object playerHand;
-        [SerializeField, RequireInterface(typeof(ICardHand))] private Object enemyHand;
-
+        [SerializeField, RequireInterface(typeof(ICardHand))] private Object[] hands;
+        
         private void Awake()
         {
-            Debug.Log(playerHand);
-            Debug.Log(enemyHand);
-            
             var cardsToPool = new Dictionary<string, GameObject>();
-            CardAdder(cardsToPool, playerHand as ICardHand);
-            CardAdder(cardsToPool, enemyHand as ICardHand);
-            //TODO: Make card amount adjust based on AP cost(i.e. pawn might need 20 units but juggernaut might only need 2)
+            AddCards(cardsToPool); 
+            CreatePools(cardsToPool); //TODO: Make card amount adjust based on AP cost(i.e. pawn might need 20 units but juggernaut might only need 2)
+        }
+
+        private void CreatePools(Dictionary<string, GameObject> cardsToPool)
+        {
             foreach (var uniqueCard in cardsToPool)
             {
                 pool.CreatePool(uniqueCard.Key, uniqueCard.Value, 10);
             }
         }
 
-        private void CardAdder(Dictionary<string, GameObject> cardsToPool, ICardHand hand)
+        private void AddCards(Dictionary<string, GameObject> cardsToPool)
+        {
+            foreach (var hand in hands)
+                AddCards(cardsToPool, hand as ICardHand);
+        }
+
+        private void AddCards(Dictionary<string, GameObject> cardsToPool, ICardHand hand)
         {
             foreach (var card in hand.Cards)
             {
-                //To avoid excessive amounts of units added, only add unit of x name once
                 if (!cardsToPool.ContainsKey(card.Name))
-                {
                     cardsToPool.Add(card.Name, card.CardObject);
-                }
             } 
         }
     }
