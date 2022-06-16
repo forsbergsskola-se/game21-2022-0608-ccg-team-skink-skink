d3e4467663ca22dc768858beed81ef5ba5375557
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-06-16 16:36:16 +0200

    Added helpful comments/todos and made a simple AI unit spawner for testing

--- Assets/Scripts/Gameplay/AI/AISpawner.cs
+++ Assets/Scripts/Gameplay/AI/AISpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AISpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject enemy;
+    public GameObject enemy2;
+ 
+    public Transform[] spawnPoints;
+ 
+    public Vector3 spawnValues;
+    public int spawnTick = 10;
+    public int spawnAmount = 2;
+    public float eTimer;
+     
+     
+    void Update () 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        eTimer = eTimer - Time.deltaTime;
+        if(eTimer<0){eTimer=spawnTick;
+ 
+            int i = spawnAmount;
+            while(i>0)
+            {
+                i--;
+                int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+                Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+                Instantiate(enemy2, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            }
+        }
         
     }
 }
--- Assets/Scripts/Utility/PoolLoader.cs
+++ Assets/Scripts/Utility/PoolLoader.cs
@@ -16,7 +16,7 @@ namespace Utility
             var cardsToPool = new Dictionary<string, GameObject>();
             CardAdder(cardsToPool, playerHand);
             CardAdder(cardsToPool, enemyHand);
-
+            //TODO: Make card amount adjust based on AP cost(i.e. pawn might need 20 units but juggernaut might only need 2)
             foreach (var uniqueCard in cardsToPool)
             {
                 pool.CreatePool(uniqueCard.Key, uniqueCard.Value, 10);
@@ -27,6 +27,7 @@ namespace Utility
         {
             foreach (var card in hand.Cards)
             {
+                //To avoid excessive amounts of units added, only add unit of x name once
                 if (!cardsToPool.ContainsKey(card.Name))
                 {
                     cardsToPool.Add(card.Name, card.CardObject);
