768971a22f08b09d936821f85e4e10549dc93534
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-06-16 11:22:04 +0200

    Update PoolSO.cs

--- Assets/Scripts/Utility/PoolSO.cs
+++ Assets/Scripts/Utility/PoolSO.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[CreateAssetMenu(menuName = "Utilitys/Pool",fileName = "NewPool")]
 
 public class PoolSO : ScriptableObject
 {
     
-  
+    public Queue<GameObject> pool;
+
+   
+
 }
