f043cca7ffa04dcc9419f0a062f8ad9033921275
TomOlovsson <tom.olovsson@student.forsbergsskola.se>
2022-06-16 16:38:29 +0200

    Created soundbanks and music events w Dramatic cue param

--- Assets/Scripts/Sound/LevelMusic.cs
+++ Assets/Scripts/Sound/LevelMusic.cs
@@ -6,20 +6,17 @@ public class LevelMusic : MonoBehaviour
 {
     [SerializeField] List<FMODUnity.EventReference> musicLevel;
     FMOD.Studio.EventInstance musicEvInst;
-
     void Start()
     {
         //Select Music event reference
         musicEvInst.start();
     }
-
-    void Update()
-    {
-        
-    }
-
     void SelectMusic(int level)
     {
         musicEvInst = FMODUnity.RuntimeManager.CreateInstance(musicLevel[level]);
+    }
+    void DramaticOrchestraCue()
+    {
+        musicEvInst.setParameterByName("DramaticCue", 1);
     }
 }
