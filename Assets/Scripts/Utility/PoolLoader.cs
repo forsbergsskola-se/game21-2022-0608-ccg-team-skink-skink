d2d20dc3944f5f7300a4254aefb9a85dd8f90dc7
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-16 18:48:55 +0200

    Refactor PoolLoader

--- Assets/Scripts/Utility/PoolLoader.cs
+++ Assets/Scripts/Utility/PoolLoader.cs
@@ -6,25 +6,21 @@ namespace Utility
 {
     public class PoolLoader : MonoBehaviour
     {
+        [Header("Unities")] 
+        [SerializeField] private int amount;
+        
+        [Header("Dependencies")]
         [SerializeField] private Pool pool;
         [SerializeField, RequireInterface(typeof(ICardHand))] private Object[] hands;
         
         private void Awake()
         {
             var cardsToPool = new Dictionary<string, GameObject>();
-            AddCards(cardsToPool); 
+            AddCHands(cardsToPool); 
             CreatePools(cardsToPool); //TODO: Make card amount adjust based on AP cost(i.e. pawn might need 20 units but juggernaut might only need 2)
         }
 
-        private void CreatePools(Dictionary<string, GameObject> cardsToPool)
-        {
-            foreach (var uniqueCard in cardsToPool)
-            {
-                pool.CreatePool(uniqueCard.Key, uniqueCard.Value, 10);
-            }
-        }
-
-        private void AddCards(Dictionary<string, GameObject> cardsToPool)
+        private void AddCHands(Dictionary<string, GameObject> cardsToPool)
         {
             foreach (var hand in hands)
                 AddCards(cardsToPool, hand as ICardHand);
@@ -37,6 +33,14 @@ namespace Utility
                 if (!cardsToPool.ContainsKey(card.Name))
                     cardsToPool.Add(card.Name, card.CardObject);
             } 
+        }
+
+        private void CreatePools(Dictionary<string, GameObject> cardsToPool)
+        {
+            foreach (var uniqueCard in cardsToPool)
+            {
+                pool.CreatePool(uniqueCard.Key, uniqueCard.Value, amount);
+            }
         }
     }
 }
