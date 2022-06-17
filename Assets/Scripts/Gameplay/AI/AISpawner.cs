5e520f8b1f3efc475e3400985412452773215fbc
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-06-17 11:22:23 +0200

    Polished the units and made AI spawner into wave spawner

--- Assets/Scripts/Gameplay/AI/AISpawner.cs
+++ Assets/Scripts/Gameplay/AI/AISpawner.cs
@@ -1,34 +1,87 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AISpawner : MonoBehaviour
 {
-    public GameObject enemy;
-    public GameObject enemy2;
- 
-    public Transform[] spawnPoints;
- 
-    public Vector3 spawnValues;
-    public int spawnTick = 10;
-    public int spawnAmount = 2;
-    public float eTimer;
-     
-     
-    void Update () 
+    public List<EnemyAI> enemies = new List<EnemyAI>();
+    public int currentWave;
+    public int waveValue;
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
+
+    public Transform spawnLocation;
+    public int waveDuration; 
+    private float waveTimer;
+    private float spawnInterval;
+    private float spawnTimer;
+
+    private void Start()
     {
-        eTimer = eTimer - Time.deltaTime;
-        if(eTimer<0){eTimer=spawnTick;
- 
-            int i = spawnAmount;
-            while(i>0)
+       GenerateWave();
+    }
+
+    void FixedUpdate () 
+    {
+        if (spawnTimer <= 0)
+        {
+            if (enemiesToSpawn.Count > 0)
             {
-                i--;
-                int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-                Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-                Instantiate(enemy2, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+                Instantiate(enemiesToSpawn[0], spawnLocation.position, Quaternion.identity);
+                enemiesToSpawn.RemoveAt(0);
+                spawnTimer = spawnInterval;
+            }
+            else
+            {
+                waveTimer = 0;
             }
         }
-        
+        else
+        {
+            spawnTimer -= Time.fixedDeltaTime;
+            waveTimer -= Time.fixedDeltaTime;
+        }
     }
+
+    public void GenerateWave()
+    {
+        waveValue = currentWave * 10;
+        GenerateEnemies();
+        
+        //provides fixed time between enemies
+        spawnInterval = waveDuration / enemiesToSpawn.Count;
+        waveTimer = waveDuration;
+    }
+
+    public void GenerateEnemies()
+    {
+        //create temp list of enemies to generate in loop
+        //if can afford, add to list and deduct cost
+        List<GameObject> generatedEnemies = new List<GameObject>();
+        while (waveValue > 0)
+        {
+            int randomEnemyID = Random.Range(0, enemies.Count);
+            int randomEnemyCost = enemies[randomEnemyID].cost;
+
+            if (waveValue - randomEnemyCost >= 0)
+            {
+                generatedEnemies.Add(enemies[randomEnemyID].enemyPrefab);
+                waveValue -= randomEnemyCost;
+            }
+            else if (waveValue <= 0)
+            {
+                break;
+            }
+        }
+        enemiesToSpawn.Clear();
+        enemiesToSpawn = generatedEnemies;
+    }
+}
+
+[System.Serializable]
+public class EnemyAI
+{
+    public GameObject enemyPrefab;
+    public int cost;
 }
