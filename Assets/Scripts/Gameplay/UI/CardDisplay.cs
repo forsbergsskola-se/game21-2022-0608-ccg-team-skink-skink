8f5fdca1798ad834ba88684c9bff56682d4933e5
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-17 08:01:48 +0200

    UIButtons display CardNames

---
+++ Assets/Scripts/Gameplay/UI/CardDisplay.cs
@@ -0,0 +1,24 @@
+using System;
+using Meta.Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace Gameplay.UI
+{
+    public class CardDisplay : MonoBehaviour
+    {
+        [Header("Dependencies")]
+        [SerializeField] private Button[] buttons;
+        [SerializeField, RequireInterface(typeof(ICardHand))] private Object playerHand;
+
+        private void Awake()
+        {
+            ICardHand hand = playerHand as ICardHand;
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].GetComponentInChildren<Text>().text = hand.Cards[i].Name;
+            }
+        }
+    }
+}
