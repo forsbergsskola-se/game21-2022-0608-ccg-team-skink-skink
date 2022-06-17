acb9f5bc847656bfe6998f73e2cab1df1a9fce0d
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-17 16:13:27 +0200

    CardHandSO refactored to be more clean.
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
    Co-Authored-By: Olle Reberg <72219278+OlleReberg@users.noreply.github.com>

--- Assets/Scripts/Meta/InventorySystem/CardHandSO.cs
+++ Assets/Scripts/Meta/InventorySystem/CardHandSO.cs
@@ -9,9 +9,21 @@ namespace Meta.InventorySystem
     public class CardHandSO : ScriptableObject, ICardHand
     {
         [SerializeField, RequireInterface(typeof(ICard))] private Object abilityCard;
-        [SerializeField, RequireInterface(typeof(ICard))] private Object[] cards;
+        [SerializeField, RequireInterface(typeof(ICard))] private Object[] cards = new Object[6];
 
-        public ICard[] Cards => Array.ConvertAll(cards, card => card as ICard);
-        public ICard AbilityCard => abilityCard as ICard;
+        public ICard[] Cards
+        {
+            get => Array.ConvertAll(cards, card => card as ICard);
+            set
+            {
+                cards = Array.ConvertAll(value, card => card as Object);
+            }
+        }
+
+        public ICard AbilityCard
+        {
+            get => abilityCard as ICard;
+            set => abilityCard = value as Object;
+        }
     }
 }
