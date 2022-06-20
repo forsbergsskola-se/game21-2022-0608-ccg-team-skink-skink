046e5aaa0bdd1cb7784387da87345871be13df00
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-20 10:50:11 +0200

    Add WaveSO to Created Assets Menu
    Co-Authored-By: Olle Reberg <72219278+OlleReberg@users.noreply.github.com>
    Co-Authored-By: Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>

--- Assets/Scripts/Gameplay/AI/WaveSO.cs
+++ Assets/Scripts/Gameplay/AI/WaveSO.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 namespace Gameplay.AI
 {
-    [CreateAssetMenu(menuName = "ScriptableObjects/Utilities", fileName = "NewWaveSo")]
-    public class WaveSo : ScriptableObject
+    [CreateAssetMenu(menuName = "ScriptableObjects/Utilities/Wave", fileName = "NewWaveSo")]
+    public class WaveSO : ScriptableObject
     {
         [SerializeField] private string[] units = new string[1];
     }
