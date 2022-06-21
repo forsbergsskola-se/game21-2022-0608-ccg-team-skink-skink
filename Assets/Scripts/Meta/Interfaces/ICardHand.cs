68c3a37b9e7e815a7a8d5ca5d8672d14dd5c6268
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-21 12:06:04 +0200

    Split up ICardHand into two interfaces
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/Interfaces/ICardHand.cs
+++ Assets/Scripts/Meta/Interfaces/ICardHand.cs
@@ -5,10 +5,14 @@ namespace Meta.Interfaces
         /// <summary>
         /// Holds the current hand of cards
         /// </summary>
-        public ICard[] Cards { get; set; }
+        public ICard[] Cards { get; }
         /// <summary>
         /// Holds the special ability card
         /// </summary>
-        public ICard AbilityCard { get; set; }
+        public ICard AbilityCard { get;}
+    }
+    public interface ISettableCardHand : ICardHand
+    {
+        public new ICard AbilityCard { set; }
     }
 }
