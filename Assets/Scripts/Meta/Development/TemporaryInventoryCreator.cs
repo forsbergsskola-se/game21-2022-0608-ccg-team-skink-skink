72104709d1f1861be374efff9b0c18a64478c119
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-21 14:59:56 +0200

    Development testing script for card viewer
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

---
+++ Assets/Scripts/Meta/Development/TemporaryInventoryCreator.cs
@@ -0,0 +1,23 @@
+using System;
+using Meta.CardSystem;
+using Meta.Interfaces;
+using UnityEngine;
+using UnityEngine.Scripting;
+using Object = UnityEngine.Object;
+namespace Meta.Development
+{
+    public class TemporaryInventoryCreator : MonoBehaviour
+    {
+        [SerializeField] InventoryViewer inventoryViewer;
+        [SerializeField, RequireInterface(typeof(ICard))] Object card;
+        void Awake()
+        {
+            var inventory = new InventoryModel();
+            for (int i = 0; i <= 10; i++)
+            {
+                inventory.Add(card as ICard);    
+            }
+            inventoryViewer.inventory = inventory;
+        }
+    }
+}
