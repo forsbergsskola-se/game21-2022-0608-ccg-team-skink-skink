5476803bb55326c58ff9bdcedf11f0401a49cf27
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-21 10:49:37 +0200

    Implmented updates from IInventory
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

--- Assets/Scripts/Meta/CardSystem/InventoryModelSO.cs
+++ Assets/Scripts/Meta/CardSystem/InventoryModelSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Meta.Interfaces;
 using UnityEngine;
@@ -7,15 +8,26 @@ namespace Meta.CardSystem
     [CreateAssetMenu(fileName = "InventoryModel", menuName = "ScriptableObjects/CardSystem/Development/InventoryModel")]
     public class InventoryModelSO : ScriptableObject, IInventory
     {
+        // TODO: Make event fire from this when the list is changed to decouple the gridview from the controller.
         public List<ICard> Cards
         {
             get;
         } = new List<ICard>();
+
+        
+        
+        private ICard selectedCard;
         
         public ICard SelectedCard
         {
-            get;
-            set;
+            get => selectedCard;
+            set
+            {
+                selectedCard = value;
+                SelectedCardChanged?.Invoke();
+            }
         }
+
+        public event Action SelectedCardChanged;
     }
 }
