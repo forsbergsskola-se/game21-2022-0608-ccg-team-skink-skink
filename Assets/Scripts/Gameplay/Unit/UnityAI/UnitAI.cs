8e57a39b5d42220c8d666a4d7bf81308f48a7e10
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-22 10:25:09 +0200

    Add Range to Unit GameObject
    Co-Authored-By: Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>

--- Assets/Scripts/Gameplay/Unit/UnityAI/UnitAI.cs
+++ Assets/Scripts/Gameplay/Unit/UnityAI/UnitAI.cs
@@ -1,11 +1,13 @@
 using System;
 using Gameplay.Unit.Health;
 using Gameplay.Unit.UnitActions;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Gameplay.Unit.UnityAI
 {
+    [RequireComponent(typeof(BoxCollider))]
     public class UnitAI : MonoBehaviour, IUnit
     {
         [Header("Dependencies")]
@@ -15,6 +17,7 @@ namespace Gameplay.Unit.UnityAI
         private Movement movement;
         private Attack attack;
         private UnitState state;
+        private BoxCollider collider;
         
         public string Target { get; set; }
         public Vector3 Direction { get; set; }
@@ -25,8 +28,11 @@ namespace Gameplay.Unit.UnityAI
             attack = new Attack(combatStats);
 
             state = UnitState.Moving;
+
+            collider = SetRange();
         }
-        
+
+
         private void FixedUpdate()
         {
             if(state == UnitState.Moving) movement.Move(transform, Direction);
@@ -40,9 +46,16 @@ namespace Gameplay.Unit.UnityAI
             if (damageReceiver != null && !other.gameObject.CompareTag(gameObject.tag))
             {
                 state = UnitState.Action;
-                Debug.Log("I am starting the attack");
                 StartCoroutine(attack.StartAttacking(damageReceiver));
             }
+        }
+
+        private BoxCollider SetRange()
+        {
+            var bc = GetComponent<BoxCollider>();
+            bc.center += new Vector3(combatStats.Range, 0, 0);
+
+            return bc;
         }
     }
 }
