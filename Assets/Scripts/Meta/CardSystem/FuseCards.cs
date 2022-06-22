2ef55cfdd18cf66f270777bc3e64047a334da47c
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-22 15:05:21 +0200

    Added first part of card fusion
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

---
+++ Assets/Scripts/Meta/CardSystem/FuseCards.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Meta.Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+public class FuseCards : MonoBehaviour
+{
+    [SerializeField, RequireInterface(typeof(ICardUpgradeScreen))] private Object cardUpgradeScreen;
+    [SerializeField] Button upgradeButton;
+    public IInventory Inventory;
+
+    void Start()
+    {
+        Inventory.SelectedCardChanged += ButtonControl;
+    }
+
+    public void ShowCardUpgradeScreen()
+    {
+        (cardUpgradeScreen as ICardUpgradeScreen).SetCard(Inventory.SelectedCard);
+    }
+    private void ButtonControl(ICard card)
+    {
+        var selectedCardList = Inventory.Cards[card.Id];
+        upgradeButton.interactable = selectedCardList.Count >= 2;
+    }
+}
