c6470bcf42bf10005277be5bf52e7d86d5402714
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-22 11:55:04 +0200

    InteractableCard.cs: Auto add subscribers
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

--- Assets/Scripts/Meta/CardSystem/InteractableCard.cs
+++ Assets/Scripts/Meta/CardSystem/InteractableCard.cs
@@ -9,6 +9,15 @@ namespace Meta.CardSystem
         public event Action<ICard> CardClicked;
         [SerializeField] BasicCardViewer basicCardViewer;
 
+        private void Awake()
+        {
+            var receivers = transform.parent.gameObject.GetComponents<ICardReceiver>();
+            foreach (var cardReceiver in receivers)
+            {
+                CardClicked += cardReceiver.ReceiveCard;
+            }
+        }
+
         public void TriggerClickedCardEvent()
         {
             CardClicked?.Invoke(basicCardViewer.Card);
