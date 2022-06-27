174496ce94b230047ccf07fa45f5a0de38c45473
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-27 17:03:21 +0200

    CardHandController.cs: Started development.
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

---
+++ Assets/Scripts/Meta/CardSystem/CardHandController.cs
@@ -0,0 +1,16 @@
+using Meta.Interfaces;
+using UnityEngine;
+
+namespace Meta.CardSystem
+{
+    public class CardHandController : MonoBehaviour
+    {
+        public IInventory Inventory;
+        public ICardHand CardHand;
+
+        private void SetCard(int index, ICard card)
+        {
+            
+        }
+    }
+}
