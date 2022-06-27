9228c62ef0b6e9b56219324ab14ec7a36918dbcb
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-06-27 17:55:18 +0200

    CardHandController now can modify the cardhand
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/CardSystem/CardHandController.cs
+++ Assets/Scripts/Meta/CardSystem/CardHandController.cs
@@ -1,16 +1,25 @@
+using System;
 using Meta.Interfaces;
 using UnityEngine;
 
 namespace Meta.CardSystem
 {
-    public class CardHandController : MonoBehaviour
+    public class CardHandController : MonoBehaviour, ICardReceiver
     {
         public IInventory Inventory;
-        public ICardHand CardHand;
-
+        public ISettableCardHand CardHand;
+        
         private void SetCard(int index, ICard card)
         {
-            
+            CardHand[index] = card;
+        }
+        
+        public  void ReceiveCard(ICard card)
+        {
+            if (Inventory.SelectedCard == null)
+                return;
+            SetCard(Array.IndexOf(CardHand.Cards, card), Inventory.SelectedCard);
+            Inventory.SelectedCard = null;
         }
     }
 }
