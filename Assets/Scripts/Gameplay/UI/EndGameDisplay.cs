0232260d1d983580c04073840215db4f9f8e209b
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-06-27 16:46:09 +0200

    Refactor EndGameDisplay

--- Assets/Scripts/Gameplay/UI/EndGameDisplay.cs
+++ Assets/Scripts/Gameplay/UI/EndGameDisplay.cs
@@ -13,27 +13,28 @@ namespace Gameplay.UI
 
         private void Start()
         {
-            endGame.SubscribeWin(() => SetWinScreen());
-            endGame.SubscribeLose(() => SetLoseScreen());
+            endGame.SubscribeWin(SetWinScreen);
+            endGame.SubscribeLose(SetLoseScreen);
         }
 
         public void SetLoseScreen()
         {
-            textMesh.gameObject.SetActive(true);
+            ActivateComponents();
             textMesh.text = "You lose!";
             textMesh.color = Color.red;
-
-            button.gameObject.SetActive(true);
-
         }
 
         private void SetWinScreen()
         {
-            textMesh.gameObject.SetActive(true);
+            ActivateComponents();
             textMesh.text = "You win!";
             textMesh.color = Color.yellow;
-            
+        }
+
+        private void ActivateComponents()
+        {
             button.gameObject.SetActive(true);
+            textMesh.gameObject.SetActive(true);
         }
     }
 }
