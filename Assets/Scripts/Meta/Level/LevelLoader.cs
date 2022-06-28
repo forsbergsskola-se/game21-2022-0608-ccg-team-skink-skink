c33daf5a2e15a0eab278a5dce35f73f08fce3334
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-06-28 14:36:35 +0200

    Loading and unloading banks respectively for level1 and metascene

--- Assets/Scripts/Meta/Level/LevelLoader.cs
+++ Assets/Scripts/Meta/Level/LevelLoader.cs
@@ -12,14 +12,4 @@ namespace Meta.Level
             FMODUnity.RuntimeManager.LoadBank(level.InternalSceneSoundBankName);
         }
     }
-
-    public class MetaSceneLoader : MonoBehaviour, ILevelController
-    {
-        public void LoadLevel(ILevel level)
-        {
-            SceneManager.LoadScene("Scenes/TestScenes/MenuTest");
-            FMODUnity.RuntimeManager.UnloadBank(level.InternalSceneSoundBankName);
-            FMODUnity.RuntimeManager.LoadBank("bank:/Mobile/MetaGame");
-        }
-    }
 }
