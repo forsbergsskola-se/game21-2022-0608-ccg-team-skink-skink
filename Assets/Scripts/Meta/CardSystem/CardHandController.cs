d8dd972466103b63c5d9fa27c12f6c3a190c0532
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-28 11:12:33 +0200

    CardHandController.cs: Completed functionality
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

--- Assets/Scripts/Meta/CardSystem/CardHandController.cs
+++ Assets/Scripts/Meta/CardSystem/CardHandController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Meta.Interfaces;
 using UnityEngine;
 
@@ -9,31 +10,32 @@ namespace Meta.CardSystem
         public IInventory Inventory;
         public ISettableCardHand CardHand;
 
+        private void Start()
+        {
+            Inventory.SelectedCardChanged += CheckIfCanSet;
+        }
 
         private void SetCard(int index, ICard card)
         {
             CardHand[index] = card;
         }
         
-        public  void ReceiveCard(ICard card)
+        public void ReceiveCard(ICard card)
         {
-            
-            
-            if (Inventory.SelectedCard == null)
+            CardHand.SelectedCard = card;
+            CheckIfCanSet(card);
+        }
+
+        private void CheckIfCanSet(ICard card)
+        {
+            if (Inventory.SelectedCard == null || CardHand.SelectedCard == null)
                 return;
             
-            // TODO:
-            /* Modify card hand criteria.
-             * 1. Selected card in inventory is not null
-             * 2. Selected card in hand is not the same as selected card in inventory.
-             * 3. Selected card in inventory is not already in another slot in the hand.
-             * 4. 
-             */
+            if (Inventory.SelectedCard != CardHand.SelectedCard && !CardHand.Cards.Contains(Inventory.SelectedCard))
+                SetCard(Array.IndexOf(CardHand.Cards, CardHand.SelectedCard), Inventory.SelectedCard);
             
-            
-            
-            SetCard(Array.IndexOf(CardHand.Cards, card), Inventory.SelectedCard);
             Inventory.SelectedCard = null;
+            CardHand.SelectedCard = null;
         }
     }
 }
