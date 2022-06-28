6ae87d90311cc5bceacbfd6538d001d751a60636
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-06-28 15:24:20 +0200

    Fixing levelloader

--- Assets/Scripts/Meta/Level/LevelLoader.cs
+++ Assets/Scripts/Meta/Level/LevelLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Meta.Interfaces;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,7 +8,12 @@ namespace Meta.Level
     {
         public void LoadLevel(ILevel level)
         {
+            StartCoroutine(Co_LoadLevel(level));
+        }
+        private static IEnumerator Co_LoadLevel(ILevel level)
+        {
             SceneManager.LoadScene(level.InternalSceneName);
+            yield return new WaitForSeconds(3);
             FMODUnity.RuntimeManager.UnloadBank("bank:/Mobile/MetaGame");
             FMODUnity.RuntimeManager.LoadBank(level.InternalSceneSoundBankName);
         }
