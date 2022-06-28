aeab36cb79ba6c21a3b93f158af35408f3671ccc
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-28 09:57:54 +0200

    CardHandController.cs: Added some todo stuffs

--- Assets/Scripts/Meta/CardSystem/CardHandController.cs
+++ Assets/Scripts/Meta/CardSystem/CardHandController.cs
@@ -8,7 +8,8 @@ namespace Meta.CardSystem
     {
         public IInventory Inventory;
         public ISettableCardHand CardHand;
-        
+
+
         private void SetCard(int index, ICard card)
         {
             CardHand[index] = card;
@@ -16,8 +17,21 @@ namespace Meta.CardSystem
         
         public  void ReceiveCard(ICard card)
         {
+            
+            
             if (Inventory.SelectedCard == null)
                 return;
+            
+            // TODO:
+            /* Modify card hand criteria.
+             * 1. Selected card in inventory is not null
+             * 2. Selected card in hand is not the same as selected card in inventory.
+             * 3. Selected card in inventory is not already in another slot in the hand.
+             * 4. 
+             */
+            
+            
+            
             SetCard(Array.IndexOf(CardHand.Cards, card), Inventory.SelectedCard);
             Inventory.SelectedCard = null;
         }
