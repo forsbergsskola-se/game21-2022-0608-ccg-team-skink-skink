e9fa04e8de68ed99e9654d203a5702142b7dfea6
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-29 17:43:08 +0200

    SellCards.cs: Improved functionality
    - Can now open ui.
    - Gets sell costs from cards instead of field.

--- Assets/Scripts/Meta/CardSystem/SellCards.cs
+++ Assets/Scripts/Meta/CardSystem/SellCards.cs
@@ -1,9 +1,8 @@
-using System;
 using Meta.Interfaces;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Utility;
+using Object = UnityEngine.Object;
 
 
 namespace Meta.CardSystem
@@ -11,11 +10,11 @@ namespace Meta.CardSystem
     public class SellCards : MonoBehaviour
     {
         [SerializeField] Button sellButton;
+
+        [SerializeField, RequireInterface(typeof(ISellCardScreen))] private Object sellCardScreen;
         
         private IInventory Inventory;
         private INormalCoinCarrier NormalCoinCarrier;
-
-        [SerializeField]int sellPrice = 100;
 
         void Awake()
         {
@@ -29,14 +28,25 @@ namespace Meta.CardSystem
             Inventory.CardAdded += ButtonControl;
         }
 
+        public void ShowSellCardScreen()
+        {
+            (sellCardScreen as ISellCardScreen).SetViewFromCard(Inventory.SelectedCard);
+        }
+
         public void SellCard()
         {
-            NormalCoinCarrier.Amount += sellPrice*Inventory.SelectedCard.CardLevel;
+            NormalCoinCarrier.Amount += Inventory.SelectedCard.SellCost;
             Inventory.Remove(Inventory.SelectedCard);
         }
+        
         public void SellAllCards()
         {
-            
+            // This is here since the count changes while we sell it, so we cache the value before starting.
+            var amountToSell = Inventory.Cards[Inventory.SelectedCard.Id].Count;
+            for (var index = 0; index < amountToSell; index++)
+            {
+                SellCard();
+            }
         }
 
         private void ButtonControl(ICard card)
