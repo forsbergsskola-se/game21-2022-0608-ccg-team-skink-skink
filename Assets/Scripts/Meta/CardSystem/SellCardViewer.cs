2e756dbb252a72e3fa3d0ee54dffb426f0795441
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-29 17:42:13 +0200

    Created ISellCardViewer implementer.

---
+++ Assets/Scripts/Meta/CardSystem/SellCardViewer.cs
@@ -0,0 +1,68 @@
+using System;
+using Meta.Interfaces;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+
+namespace Meta.CardSystem
+{
+    public class SellCardViewer : MonoBehaviour, ISellCardScreen
+    {
+        [SerializeField] private Image cardImage;
+        [SerializeField] private TMP_Text cardNameText;
+        [SerializeField] private TMP_Text cardLevelText;
+        [SerializeField] private TMP_Text singleCostText;
+        [SerializeField] private TMP_Text allCostText;
+
+        private IInventory inventory;
+
+        public void SetViewFromCard(ICard card)
+        {
+            if (card == null)
+            {
+                Hide();
+                return;
+            }
+
+            gameObject.SetActive(true);
+            
+            cardImage.sprite = card.CardImage;
+            cardNameText.text = card.Name;
+            cardLevelText.text = card.CardLevel.ToString();
+            singleCostText.text = card.SellCost.ToString();
+            allCostText.text = (card.SellCost * inventory.Cards[card.Id].Count).ToString();
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void Awake()
+        {
+            inventory = Dependencies.Instance.Inventory;
+        }
+
+        private void Start()
+        {
+            inventory.CardRemoved += UpdateAllCost;
+        }
+
+        private void UpdateAllCost(ICard card)
+        {
+            if (RanOutOfCards(card))
+            {
+                SetViewFromCard(null);
+                return;
+            }
+            
+            allCostText.text = (card.SellCost * inventory.Cards[card.Id].Count).ToString();
+        }
+
+        private bool RanOutOfCards(ICard card)
+        {
+            return !inventory.Cards.ContainsKey(card.Id);
+        }
+    }
+}
