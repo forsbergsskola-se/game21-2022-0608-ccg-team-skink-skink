5f34d88e946d66ee118e3c5c6f2e1f0c3937ecc7
TomOlovsson <tom.olovsson@student.forsbergsskola.se>
2022-06-30 14:30:06 +0200

    Updated music script
    - Changed from list to single variable

--- Assets/Scripts/Sound/LevelMusic.cs
+++ Assets/Scripts/Sound/LevelMusic.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class LevelMusic : MonoBehaviour
 {
-    [SerializeField] List<FMODUnity.EventReference> musicLevel;
+    [SerializeField] FMODUnity.EventReference musicLevel;
     FMOD.Studio.EventInstance musicEvInst;
     void Start()
     {
         //Select Music event reference
         musicEvInst.start();
-    }
-    void SelectMusic(int level)
-    {
-        musicEvInst = FMODUnity.RuntimeManager.CreateInstance(musicLevel[level]);
     }
     void DramaticOrchestraCue()
     {
