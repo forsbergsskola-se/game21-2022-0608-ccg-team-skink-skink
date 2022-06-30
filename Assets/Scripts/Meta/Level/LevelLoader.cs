75de950972a5e90c8e39220c220a02d85dd279b5
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-06-30 13:56:40 +0200

    Removed redundant code
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/Level/LevelLoader.cs
+++ Assets/Scripts/Meta/Level/LevelLoader.cs
@@ -6,13 +6,6 @@ namespace Meta.Level
 {
     public class LevelLoader : MonoBehaviour, ILevelController
     {
-        private string metaBank = "Mobile/MetaGame";
-        private string menuBank = "Mobile/Master";
-
-        private string gameBank = "Mobile/Game";
-        //TODO: Switch to "MetaGame", once that scene has been implemented
-        private string metaSceneName = "MetaLevelTest";
-        private string menuSceneName = "MenuTest";
         public void LoadLevel(ILevel level)
         {
             TestLoadLevel(level.InternalSceneSoundBankName, level.InternalSceneName);
@@ -22,34 +15,6 @@ namespace Meta.Level
         {
             SceneManager.LoadScene(nextLevelName);
             FMODUnity.RuntimeManager.LoadBank(soundBank);
-        }
-        
-        //TODO: Check if should maybe be static(bc marc said so?)
-        //TODO: Remove code below, if deemed redundant
-        private IEnumerator Co_LoadLevel(ILevel level)
-        {
-            SceneManager.LoadScene(level.InternalSceneName);
-            if (level.Name == metaSceneName)
-            {
-                FMODUnity.RuntimeManager.LoadBank(metaBank);
-                yield return new WaitForSeconds(1);
-                // FMODUnity.RuntimeManager.UnloadBank(gameBank);
-                // FMODUnity.RuntimeManager.UnloadBank(level.InternalSceneSoundBankName);
-            }
-            else if (level.Name == menuSceneName)
-            {
-                FMODUnity.RuntimeManager.LoadBank(menuBank);
-                yield return new WaitForSeconds(1);
-                // FMODUnity.RuntimeManager.UnloadBank(level.InternalSceneSoundBankName);
-                // FMODUnity.RuntimeManager.UnloadBank(gameBank);
-            }
-            else
-            {
-                FMODUnity.RuntimeManager.LoadBank(gameBank);
-                FMODUnity.RuntimeManager.LoadBank(level.InternalSceneSoundBankName);
-                yield return new WaitForSeconds(1);
-                // FMODUnity.RuntimeManager.UnloadBank(metaBank);
-            }
         }
     }
 }
