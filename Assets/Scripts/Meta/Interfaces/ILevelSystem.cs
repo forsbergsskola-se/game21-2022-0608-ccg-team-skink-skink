1389b202fa37c4e78dfab8ccf4027d3a73e1a935
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-06-30 13:54:55 +0200

    Split up interfaces into menu levels and gameplay levels
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/Interfaces/ILevelSystem.cs
+++ Assets/Scripts/Meta/Interfaces/ILevelSystem.cs
@@ -12,11 +12,15 @@ namespace Meta.Interfaces
     }
     public interface ILevel
     {
-        //TODO: Either find a use for or scrap Index, EnemyHandPreset(and add some other way), and Name
         public string Name { get; }
         public int Index { get; }
-        public ICardHand EnemyHandPreset { get; }
         public string InternalSceneName { get; }
         public string InternalSceneSoundBankName { get; }
+    }
+
+    public interface IGameplayLevel : ILevel
+    {
+        public ICardHand EnemyHandPreset { get; }
+        public string GameSoundBank { get; }
     }
 }
