e44ac4a5f338b565c94bfa5e9cc235cfa7e1240d
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-07-01 14:20:14 +0200

    LootBoxSystemController.cs: Complete functionality
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

--- Assets/Scripts/Meta/LootBox/LootBoxSystemController.cs
+++ Assets/Scripts/Meta/LootBox/LootBoxSystemController.cs
@@ -1,34 +1,49 @@
 using Meta.Interfaces;
 using UnityEngine;
+using UnityEngine.UI;
 using Utility;
+using Object = UnityEngine.Object;
 
 
 namespace Meta.LootBox
 {
     public class LootBoxSystemController : MonoBehaviour
     {
-        private ILootBoxInventoryModel lootBoxInventoryModel;
-
-        [SerializeField, RequireInterface(typeof(ILootBoxViewer))] private Object lootBoxViewer;
+        [SerializeField, RequireInterface(typeof(ILootBoxInventoryModel))] private Object lootBoxInventoryModel;
+        [SerializeField] private Button lootBoxClickButton;
+        private IInventory inventory;
+        private ILootBoxAmountModel lootBoxAmountModel;
+        
 
         private void Awake()
         {
-            
+            inventory = Dependencies.Instance.Inventory;
+            lootBoxAmountModel = Dependencies.Instance.LootBoxAmountModel;
+        }
+
+        private void OnEnable()
+        {
+            lootBoxAmountModel.ValueChanged += ButtonControl;
+            ButtonControl(lootBoxAmountModel.Amount);
+        }
+
+        private void OnDisable()
+        {
+            lootBoxAmountModel.ValueChanged -= ButtonControl;
         }
 
         public void OpenLootBox()
         {
-            
+            var cardsReceived = (lootBoxInventoryModel as ILootBoxInventoryModel).OpenLootBox();
+            foreach (var card in cardsReceived)
+            {
+                inventory.Add(card);
+            }
         }
 
-        public void EnableLootBoxScreen()
+        private void ButtonControl(int i)
         {
-        
-        }
-
-        public void DisableLootBoxScreen()
-        {
-        
+            lootBoxClickButton.interactable = i > 0;
         }
     }
 }
