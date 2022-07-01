b9c5d12d448dee840a0d8b25a12928fbba323631
Arvid Lundin <arvid.lundin@student.forsbergsskola.se>
2022-07-01 10:01:38 +0200

    created snapshot for snapshot

--- Assets/Scripts/Sound/LevelMusic.cs
+++ Assets/Scripts/Sound/LevelMusic.cs
@@ -6,6 +6,11 @@ public class LevelMusic : MonoBehaviour
 {
     [SerializeField] FMODUnity.EventReference music;
     FMOD.Studio.EventInstance musicEvInst;
+
+    //Audio filter for when the game is paused
+    public FMODUnity.EventReference pauseFilterRef;
+    private FMOD.Studio.EventInstance pauseFilterInst;
+
     void Start()
     {
         musicEvInst = FMODUnity.RuntimeManager.CreateInstance(music);
@@ -16,5 +21,10 @@ public class LevelMusic : MonoBehaviour
         Debug.Log("DramaticCueSound");
         musicEvInst.setParameterByName("DramaticCue", 1);
         
+    }
+    public void PauseMenuAudio()
+    {
+        pauseFilterInst = FMODUnity.RuntimeManager.CreateInstance("snapshot:/PauseFilter");
+        pauseFilterInst.start();
     }
 }
