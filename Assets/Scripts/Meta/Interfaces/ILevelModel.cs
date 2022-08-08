7d6c3dd0d6a9ff0686fa5e40fb919e1daeab4f4c
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-08-08 15:45:47 +0200

    ILevelModel.cs: Added event for value changes
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/Interfaces/ILevelModel.cs
+++ Assets/Scripts/Meta/Interfaces/ILevelModel.cs
@@ -1,7 +1,9 @@
-﻿namespace Meta.Interfaces 
+﻿using System;
+namespace Meta.Interfaces 
 {
     public interface ILevelsModel
     {
+        public event Action<int> MaxLevelIndexChanged; 
         public int CurrentMaxLevelIndex { get; set; }
         public IGameplayLevel CurrentLevel { get; set; }
     }
