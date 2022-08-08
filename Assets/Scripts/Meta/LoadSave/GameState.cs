1960d0f905bb97c45d7b47f2a575d85b84f4577e
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-08-08 15:35:01 +0200

    Add LootBoxesAmount to GameState
    Co-Authored-By: Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>

--- Assets/Scripts/Meta/LoadSave/GameState.cs
+++ Assets/Scripts/Meta/LoadSave/GameState.cs
@@ -1,10 +1,12 @@
-using System;
-
 namespace Meta.LoadSave
 {
     public class GameState
     {
         public int currency;
-        public override string ToString() => $"Currency: {currency}\n";
+        public int lootBoxesAmount;
+        
+        
+        public override string ToString() => $"Currency: {currency}\n" +
+                                             $"LootBoxesAmount: {lootBoxesAmount}\n";
     }
 }
