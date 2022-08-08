514b2446698696da50f11f4d2328619b92e5aa33
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-08-08 15:40:17 +0200

    Add CardHand
    Co-Authored-By: Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>

--- Assets/Scripts/Meta/LoadSave/GameState.cs
+++ Assets/Scripts/Meta/LoadSave/GameState.cs
@@ -5,10 +5,12 @@ namespace Meta.LoadSave
         public int currency;
         public int lootBoxesAmount;
         public int currentLevel;
+        public int[] cardHand = new int[6];
         
         
         public override string ToString() => $"Currency: {currency}\n" +
                                              $"LootBoxesAmount: {lootBoxesAmount}\n" +
-                                             $"Current Level: {currentLevel}\n";
+                                             $"Current Level: {currentLevel}\n" + 
+                                             $"CardHand: {cardHand.Length}\n";
     }
 }
