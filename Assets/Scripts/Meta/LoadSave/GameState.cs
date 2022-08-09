8348e0af1e16b73440868c5a310fef67184dad3b
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-08-09 08:34:27 +0200

    Add InventoryAmount and InventoryID

--- Assets/Scripts/Meta/LoadSave/GameState.cs
+++ Assets/Scripts/Meta/LoadSave/GameState.cs
@@ -6,11 +6,14 @@ namespace Meta.LoadSave
         public int lootBoxesAmount;
         public int currentLevel;
         public int[] cardHand = new int[6];
-        
-        
+        public int[] inventoryID = new int[32];
+        public int[] inventoryAmount = new int[32];
+
         public override string ToString() => $"Currency: {currency}\n" +
                                              $"LootBoxesAmount: {lootBoxesAmount}\n" +
-                                             $"Current Level: {currentLevel}\n" + 
-                                             $"CardHand: {cardHand.Length}\n";
+                                             $"Current Level: {currentLevel}\n" +
+                                             $"CardHand: {cardHand.Length}\n" +
+                                             $"Inventory: {inventoryID.Length}\n" +
+                                             $"Inventory: {inventoryAmount.Length}\n";
     }
 }
--- Assets/Scripts/Meta/LoadSave/InventoryUnit.cs
+++
@@ -1,8 +0,0 @@
-namespace Meta.LoadSave
-{
-    public struct InventoryUnit
-    {
-        public int id;
-        public int amount;
-    }
-}
