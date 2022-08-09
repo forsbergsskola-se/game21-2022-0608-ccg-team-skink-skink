3651978335d9f56b978091f6cf30e0a5d424967e
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-08-09 09:58:29 +0200

    Created and completed LevelMenuController
    Handles the collection all available levels. Also handles starting the detailed level viewer.

---
+++ Assets/Scripts/Meta/Level/LevelMenuController.cs
@@ -0,0 +1,32 @@
+using System;
+using Meta.Interfaces;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Meta.Level
+{
+    public class LevelMenuController : MonoBehaviour
+    {
+        [SerializeField, RequireInterface(typeof(GameplayLevelSO))] private Object[] levelsInOrder;
+
+        [SerializeField, RequireInterface(typeof(IDetailedLevelViewer))] private Object detailedLevelViewer;
+
+        private GameplayLevelSO[] levels;
+
+        private void Awake()
+        {
+            levels = Array.ConvertAll(levelsInOrder, input => input as GameplayLevelSO);
+
+            var basicLevelViewers = GetComponentsInChildren<IBasicLevelViewer>();
+            foreach (var basicLevelViewer in basicLevelViewers)
+            {
+                basicLevelViewer.OnClick += OpenDetailedLevelViewByIndex;
+            }
+        }
+
+        private void OpenDetailedLevelViewByIndex(int index)
+        {
+            (detailedLevelViewer as IDetailedLevelViewer).ActivateDetailedLevelViewer(levels[index]);
+        }
+    }
+}
