e95caeaa7752b093ef6c6b00dfe56d316ae7642f
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-08-09 08:45:32 +0200

    Refactor

--- Assets/Scripts/Meta/LoadSave/LoadState.cs
+++ Assets/Scripts/Meta/LoadSave/LoadState.cs
@@ -1,23 +1,41 @@
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Utility;
 
 namespace Meta.LoadSave
 {
     public class LoadState : MonoBehaviour
     {
-        private GameState gameState = new ();
+        private GameState gameState;
+        
         private void Awake()
         {
-            string json = File.ReadAllText(Application.dataPath + "/SavedGames/Test_01.json");
-            gameState = JsonUtility.FromJson<GameState>(json);
-            
-            Debug.Log(gameState);
+            gameState = GetGameState();
+            SetGameState(gameState);
+        }
 
-            for (int i = 0; i < gameState.inventoryID.Length; i++)
+        private GameState GetGameState()
+        {
+            string json = File.ReadAllText(Application.dataPath + "/SavedGames/Test_01.json");
+            var fromJson = JsonUtility.FromJson<GameState>(json);
+
+            Debug.Log(fromJson);
+
+            for (int i = 0; i < fromJson.inventoryID.Length; i++)
             {
-                Debug.Log($"ID: {gameState.inventoryID[i]} | Amount: {gameState.inventoryAmount[i]}");
+                Debug.Log($"ID: {fromJson.inventoryID[i]} | Amount: {fromJson.inventoryAmount[i]}");
             }
+
+            return fromJson;
+        }
+
+        private void SetGameState(GameState gameState)
+        {
+            //Todo: Add Currency
+            //Todo: Add LootBoxesAmount
+            //Todo: Add Current Level
+            //Todo: Add CardHand
+            //Todo: Add Inventory ID and Amount
         }
     }
 }
