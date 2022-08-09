ba53a3db9e1ea7345606e94fbe625ddf6837e276
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-08-09 10:09:55 +0200

    Added CardArray SO
    Co-Authored-By: Sokratis Papageorgiou <hello@sokpapageorgiou.com>
    Co-Authored-By: Olle Reberg <72219278+OlleReberg@users.noreply.github.com>

---
+++ Assets/Scripts/Utility/CardArraySO.cs
@@ -0,0 +1,14 @@
+using Meta.Interfaces;
+using UnityEngine;
+
+namespace Utility
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Utilities/CardArray",fileName = "NewCardArray")]
+    public class CardArraySO : ScriptableObject
+    {
+        [SerializeField, RequireInterface(typeof(ICard))]
+        private Object[] cards;
+
+        public ICard GetCard(int id) => cards[id] as ICard;
+    }
+}
