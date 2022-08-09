eacbc52f41b916691adc388cb8c32084c6ee0453
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-08-09 10:32:10 +0200

    Completed DetailedLevelView with activate and deactivate methods
    Co-Authored-By: Oskar Anderson <90617805+Zargn@users.noreply.github.com>

--- Assets/Scripts/Meta/Level/DetailedLevelView.cs
+++ Assets/Scripts/Meta/Level/DetailedLevelView.cs
@@ -7,10 +7,15 @@ namespace Meta.Level
     public class DetailedLevelView : MonoBehaviour, IDetailedLevelViewer
     {
         [SerializeField] private TMP_Text levelDescriptionTextBox;
+        [SerializeField] private TMP_Text levelNameTextBox;
 
-        public void ActivateDetailedLevelViewer(IGameplayLevel gameplayLevel)
-        {
-            
+        public void ActivateDetailedLevelViewer(IGameplayLevel gameplayLevel) {
+            this.gameObject.SetActive(true);
+            levelDescriptionTextBox.text = gameplayLevel.Description;
+            levelNameTextBox.text = gameplayLevel.Name;
+        }
+        public void DeactivateDetailedLevelViewer() {
+            this.gameObject.SetActive(false);
         }
     }
 }
