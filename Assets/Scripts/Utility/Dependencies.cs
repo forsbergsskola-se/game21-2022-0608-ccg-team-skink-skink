e14219eb07d775c0f14c2ee0d672898cc3a215be
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-08-10 11:11:52 +0200

    Dependencies: Added EndOfGameRelay.
    Added private LevelProgression class instance for handling background task on level wins.
    
    Co-Authored-By: Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>

--- Assets/Scripts/Utility/Dependencies.cs
+++ Assets/Scripts/Utility/Dependencies.cs
@@ -3,6 +3,7 @@ using Meta.CurrencySystem;
 using Meta.Interfaces;
 using Meta.Level;
 using Meta.LootBox;
+using Meta.Relays;
 using UnityEngine;
 
 namespace Utility
@@ -16,6 +17,10 @@ namespace Utility
         public readonly ILootBoxAmountModel LootBoxAmountModel = new LootBoxAmountModel();
         public readonly ILevelLoader LevelLoader = new LevelLoader();
         public readonly ILevelsModel LevelsModel = new LevelsModel();
+        public readonly IEndOfGame EndOfGameRelay = new EndOfGameRelay();
+        
+        // LevelProgression is only a self contained logic script that reacts to event inside some of the dependencies.
+        private LevelProgression levelProgression = new LevelProgression();
         
         
         public static Dependencies Instance { get; private set; }
