f8407d8ed4d4e2dde6c5607a509ea83e2dc2614d
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-08-15 15:54:23 +0200

    code improved

--- Assets/ActionTrigger.cs
+++ Assets/ActionTrigger.cs
@@ -1,28 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ActionTrigger : MonoBehaviour
 {
   [SerializeField] private Animator Anim;
   
-  public void Action()
-  {
-    Anim.SetTrigger("Action");
-  }
-
-  public void Walk()
-  {
-    Anim.SetBool("IsWalking", true);
-  }
+  public void Action() => Anim.SetTrigger("Action");
   
-  public void Stop()
-  {
-    Anim.SetBool("IsWalking", false);
-  }
+  public void Walk() => Anim.SetBool("IsWalking", true);
+  
+  public void Stop() => Anim.SetBool("IsWalking", false);
 
-  public void Death()
-  {
-    Anim.SetTrigger("Death");
-  }
+  public void Death() => Anim.SetTrigger("Death");
+  
 }
