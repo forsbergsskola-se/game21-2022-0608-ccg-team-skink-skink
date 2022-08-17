2c3a9dbc11366b1db51fb43ae683a2e0b225ea1c
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-08-17 16:44:21 +0200

    main menu animation coming from the hub

--- Assets/Scripts/Meta/Interfaces/ButtonAlphaMain.cs
+++ Assets/Scripts/Meta/Interfaces/ButtonAlphaMain.cs
@@ -3,28 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonAlpha : MonoBehaviour
+public class ButtonAlphaMain : MonoBehaviour
 {
+    [SerializeField] private bool mainmenu;
     [SerializeField] private CanvasGroup close;
     [SerializeField] private CanvasGroup open;
-
+    [SerializeField] private Animator anim;
     private string soundEventPath = "event:/ButtonPress";
     
+
     public void Press()
     {
         open.alpha = 1;
+        if(mainmenu)
+            anim.SetTrigger("Close");
+        else
+            anim.SetTrigger("Open");
         close.interactable = false;
         close.blocksRaycasts = false;
         
-        close.alpha = 0;
-        open.interactable = true;
-        open.blocksRaycasts = true;
         PlayButtonSound();
     }
     void PlayButtonSound()
     {
         FMODUnity.RuntimeManager.PlayOneShot(soundEventPath);
     }
-    
+
+    public void HideMainScreen()
+    {
+        close.alpha = 0;
+        open.interactable = true;
+        open.blocksRaycasts = true;
+    }
+
+    public void HideHubScreen()
+    {
+        close.alpha = 1;
+        close.interactable = true;
+        close.blocksRaycasts = true;
+    }
     
 }
