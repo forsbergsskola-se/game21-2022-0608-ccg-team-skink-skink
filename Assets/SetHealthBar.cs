c4777639fc59b53297843b89d25a80d5648d3a69
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-08-18 14:22:21 +0200

    Healthbar gradient

--- Assets/SetHealthBar.cs
+++ Assets/SetHealthBar.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class SetHealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
     public HealthComponent HealthComponent;
+    public Image fill;
 
     private void Start()
     {
         slider.maxValue = HealthComponent.HealthStats.MaxHealth;
+        
         UpdateHealthBar();
         HealthComponent.OnDamageTaken.AddListener(UpdateHealthBar);
     }
@@ -18,5 +21,6 @@ public class SetHealthBar : MonoBehaviour
     private void UpdateHealthBar()
     {
         slider.value = HealthComponent.CurrentHealth;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
