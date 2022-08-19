244968f99d9e00d9dd8a6d2f704e690f386505fd
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-08-19 09:32:01 +0200

    UnitCooldownTimer.cs: Cleanup.

--- Assets/Scripts/Gameplay/Unit/UnitUtility/UnitCoolDownTimer.cs
+++ Assets/Scripts/Gameplay/Unit/UnitUtility/UnitCoolDownTimer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Meta.Interfaces;
 using UnityEngine;
@@ -9,70 +8,57 @@ namespace Gameplay.Unit.UnitUtility
 {
     public class UnitCoolDownTimer : MonoBehaviour
     {
-        private ICardHand deckHand; // Changed to Dependency Injection
         [SerializeField] private sbyte animationFramerate = 30;
+        
+        private ICardHand deckHand;
         private float frameTime;
 
+        
         private void Start()
         {
             frameTime = 1 / (float)animationFramerate;
+            deckHand = Dependencies.Instance.PlayerCardHand;
         }
-
+        
+        
         public void CoolDownUnit(int buttonId)
         {
-            deckHand = FindObjectOfType<Dependencies>().PlayerCardHand;
-            
             GameObject clickedButtonGO = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
-            ICardHand hand = deckHand as ICardHand;
-            // StartCoroutine(UnitCoolDown(clickedButtonGO, hand.Cards[buttonId].CoolDownTime));
-            StartCoroutine(CardAnimation(clickedButtonGO, hand.Cards[buttonId].CoolDownTime));
+            StartCoroutine(CardAnimation(clickedButtonGO, deckHand.Cards[buttonId].CoolDownTime));
         }
         
-        // private IEnumerator UnitCoolDown(GameObject cardButton,int coolDownTime)
-        // {
-        //     cardButton.GetComponent<Button>().enabled = false;
-        //     CardAnimation(cardButton, coolDownTime);
-        //     //TODO: Animation + sound (in a separated script ??)
-        //     yield return new WaitForSeconds(coolDownTime);
-        //     PlayOneShot.ActivateCardAudio();
-        //     cardButton.GetComponent<Button>().enabled = true;
-        // }
         
-        
-
         private IEnumerator CardAnimation(GameObject cardObject, int coolDown)
         {
             cardObject.GetComponent<Button>().interactable = false;
             
-            var transform = cardObject.GetComponent<RectTransform>();
-            var startPos = transform.anchoredPosition;
-            var height = transform.rect.height;
+            var cardTransform = cardObject.GetComponent<RectTransform>();
+            var startPos = cardTransform.anchoredPosition;
+            var height = cardTransform.rect.height;
             var stepSize = 1f / (coolDown * animationFramerate);
             var xPos = startPos.x;
-            var yPos = startPos.y;
 
-            // Debug.Log($"Transform: {transform}, startPos: {startPos}, height: {height}, stepSize: {stepSize}");
-
+            // Animation and timer.
             float time = 1;
             do
             {
-                yPos = startPos.y + (height*0.75f) * AnimationFormula(time, coolDown * 4);
-                transform.anchoredPosition = new Vector2(xPos, yPos);
+                var yPos = startPos.y + (height*0.75f) * AnimationFormula(time, coolDown * 4);
+                cardTransform.anchoredPosition = new Vector2(xPos, yPos);
                 time -= stepSize;
                 yield return new WaitForSeconds(frameTime);
-                // Debug.Log($"time = {time}");
-
             } while (time > 0);
 
-            transform.anchoredPosition = startPos;
+            cardTransform.anchoredPosition = startPos;
             PlayOneShot.ActivateCardAudio();
             cardObject.GetComponent<Button>().interactable = true;
         }
+
 
         /// <summary>
         /// Returns a float to be used as a multiplier for changing card height.
         /// </summary>
         /// <param name="x">Any value between 1 - 0 for start to end of animation.</param>
+        /// <param name="fallSpeed">The speed of the initial bounce up and fall down.</param>
         /// <returns></returns>
         private float AnimationFormula(float x, float fallSpeed)
         {
